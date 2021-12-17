bd998e58f5b2f8f96d85bd1ae930254324a0d281
Ahmed Gamal <amgamal200111@gmail.com>
2021-12-17 14:33:37 +0200

    1

--- server.py
+++ server.py
@@ -1,1 +1,11 @@
-import flask
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "hello world"
+
+
+if __name__ == '__main__':
+    app.run()
