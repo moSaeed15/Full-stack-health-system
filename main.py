454f28f745b1c417fff253b15bd9db3a95f3ce7d
moh151 <67608414+moh151@users.noreply.github.com>
2021-12-31 14:35:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from flask import Flask , render_template, redirect
+from flask import Flask , render_template
 from flask.templating import render_template
 # from passlib.hash import sha256_crypt
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('signin.html')
-
-@app.route('/home')
-def home():
-    return render_template('/home.html')
+    return render_template('signin.html')     
 
 @app.route('/doctor')
 def doctor():
@@ -19,27 +15,30 @@ def doctor():
 def nurse():
     return render_template('nurse.html')     
 
-@app.route('/signin')
-def signin():
-    return render_template('signin.html')     
+@app.route('/home')
+def home():
+    return render_template('home.html')     
 
 @app.route('/patient')
 def patient():
     return render_template('patient.html')     
-
-
-@app.route('/machine')
-def machine():
-    return render_template('machines.html')     
-
-
+ 
 @app.route('/technician')
 def technician():
     return render_template('technician.html')     
 
-@app.route('/room')
-def rooms():
-    return render_template('rooms.html')     
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')    
+
+@app.route('/admin/add-nurse')
+def addnurse():
+    return render_template('addnurse.html')    
+
+
+@app.route('/admin/add-doctor')
+def addDoctor():
+    return render_template('addDoctor.html')    
 
 if __name__=='__main__':
     app.run(debug=True)
