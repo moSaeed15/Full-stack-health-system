cddd9f6a9c068a06bcad346608fbcab2403a39c2
Ahmed Gamal <amgamal200111@gmail.com>
2021-12-31 12:50:27 +0200

    commit on 'main'

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, redirect
 from flask.templating import render_template
 # from passlib.hash import sha256_crypt
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')     
+    return render_template('signin.html')
+
+@app.route('/home')
+def home():
+    return render_template('/home.html')
 
 @app.route('/doctor')
 def doctor():
