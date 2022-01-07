39c077cf44c9824f217359e2666c668183c9a1a6
Ahmed Gamal <amgamal200111@gmail.com>
2022-01-07 14:28:14 +0200

    .

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 from flask.templating import render_template
 import mysql.connector
 from DB import connect
+from datetime import datetime
 connect()
 
 mydb = mysql.connector.connect(
