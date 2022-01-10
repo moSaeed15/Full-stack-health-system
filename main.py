30ccc1f32a2ba6e2473c570fa21417fe19677d26
moh151 <67608414+moh151@users.noreply.github.com>
2022-01-10 13:38:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -516,6 +516,14 @@ def tchecks():
 def tissues():
 	return render_template('tissues.html') 
 
+@app.route('/about')
+def about():
+	return render_template('about.html') 
+
+@app.route('/contact')
+def contact():
+	return render_template('contact.html') 
+
 
 if __name__=='__main__':
 	app.run(debug=True)
